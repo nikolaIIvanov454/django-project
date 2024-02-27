d5b04c1c9d0b467b17e4f4cd626297a9f36816af
Nikolai Ivanov <nikitoi_vanov@abv.bg>
2024-02-27 12:14:43 +0200

    Updated commit

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+
+class HomePageTest(TestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get('/')
+        self.assertEquals(response.status_code, 200)
+
+class AboutPageTest(TestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get('/about-me/')
+        self.assertEqual(response.status_code, 200)
